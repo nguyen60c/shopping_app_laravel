46c5f71e142f129c99f1fcf2789d634ed50536ad
nguyen60c <nguyen60c@gmail.com>
2022-07-15 16:46:18 +0700

    dashboard admin screens

---
+++ resources/views/partials/aside.blade.php
@@ -0,0 +1,24 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <div class="sidebar">
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a href="#" class="d-block">{{ auth()->user()->name }}</a>
+            </div>
+        </div>
+
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>DashBoard</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+
+</aside>
