0d9c86de625199a7c162f71c6b4b52352ad6d20c
nguyen60c <nguyen60c@gmail.com>
2022-07-22 20:27:19 +0700

    update migrations

---
+++ database/seeders/RolesPermissionsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesPermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin_role = Role::create(['name' => 'admin']);
+        $admin_role->givePermissionTo(Permission::all());
+
+        $user_role = Role::create(['name' => 'user']);
+        $user_permissions = Permission::all()->filter(function ($permission) {
+            return substr($permission->name, 0, 5) != 'user_' && substr($permission->name, 0, 5) != 'role_' && substr($permission->name, 0, 11) != 'permission_';
+        });
+        $user_role->givePermissionTo($user_permissions);
+    }
+}
